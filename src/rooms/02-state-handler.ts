f4ed3184f96318e7aa4d53417ddec6ebf3cf8bf8
rPbl3yH <tnv2205@gmail.com>
2024-09-01 12:55:00 +0300

    feat: rotation

--- src/rooms/02-state-handler.ts
+++ src/rooms/02-state-handler.ts
@@ -3,7 +3,11 @@ import { Schema, type, MapSchema } from "@colyseus/schema";
 
 export class Player extends Schema {
 
+    @type("number")
+    speed = 0;
+
     planeSize = 10;
+
     @type("number")
     pX = Math.floor(Math.random() * this.planeSize) - this.planeSize / 2;
 
@@ -21,6 +25,12 @@ export class Player extends Schema {
 
     @type("number")
     vZ = 0;
+
+    @type("number")
+    rX = 0;
+
+    @type("number")
+    rY = 0;
 }
 
 export class State extends Schema {
@@ -29,8 +39,11 @@ export class State extends Schema {
 
     something = "This attribute won't be sent to the client-side";
 
-    createPlayer(sessionId: string) {
-        this.players.set(sessionId, new Player());
+    createPlayer(sessionId: string, data: any) {
+        const player = new Player();
+        player.speed = data.speed;
+
+        this.players.set(sessionId, player);
     }
 
     removePlayer(sessionId: string) {
@@ -38,13 +51,18 @@ export class State extends Schema {
     }
 
     movePlayer (sessionId: string, data: any) {
-        this.players.get(sessionId).pX = data.pX;
-        this.players.get(sessionId).pY = data.pY;
-        this.players.get(sessionId).pZ  = data.pZ;
+        const player = this.players.get(sessionId);
 
-        this.players.get(sessionId).vX = data.vX;
-        this.players.get(sessionId).vY = data.vY;
-        this.players.get(sessionId).vZ  = data.vZ;
+        player.pX = data.pX;
+        player.pY = data.pY;
+        player.pZ  = data.pZ;
+
+        player.vX = data.vX;
+        player.vY = data.vY;
+        player.vZ  = data.vZ;
+    
+        player.rX = data.rX;
+        player.rY = data.rY;
     }
 }
 
@@ -66,9 +84,9 @@ export class StateHandlerRoom extends Room<State> {
         return true;
     }
 
-    onJoin (client: Client) {
+    onJoin (client: Client, data: any) {
         client.send("hello", "world");
-        this.state.createPlayer(client.sessionId);
+        this.state.createPlayer(client.sessionId, data);
     }
 
     onLeave (client) {
@@ -78,5 +96,4 @@ export class StateHandlerRoom extends Room<State> {
     onDispose () {
         console.log("Dispose StateHandlerRoom");
     }
-
 }
